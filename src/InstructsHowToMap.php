dc666c8e16b1163e7f73482516f27f74ccc1c1da
Stratadox <info@stratadox.com>
2018-04-03 23:00:19 +0200

    Add newline between param and return annotations

--- src/InstructsHowToMap.php
+++ src/InstructsHowToMap.php
@@ -17,6 +17,7 @@ interface InstructsHowToMap
      * Follow the instruction for a property to obtain the property mapping.
      *
      * @param string $property The property to apply this instruction to.
+     *
      * @return MapsProperty    The resulting property mapping.
      */
     public function followFor(string $property): MapsProperty;
